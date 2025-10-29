4ce45714c5a9391722ecf29fab2b937d2d5856b5
Thomas Ryan <tryan3126@gmail.com>
2025-10-28 20:28:07 -0400

    Password Addition, Signup, and basic security
    This adds some key functionality.

--- models.py
+++ models.py
@@ -1,11 +1,20 @@
 from decimal import Decimal
 from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash  # NEW
 
 db = SQLAlchemy()
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), unique=True, nullable=False)
+    password_hash = db.Column(db.String(255), nullable=False)  # NEW
+
+    # helpers
+    def set_password(self, password: str) -> None:
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password: str) -> bool:
+        return check_password_hash(self.password_hash, password)
 
 class Ticker(db.Model):
     id = db.Column(db.Integer, primary_key=True)
